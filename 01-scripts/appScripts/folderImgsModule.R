1d7199cc8f559f95efcd9efee6201d827dc19d38
Amanda Gibson <gibsonag@umich.edu>
2021-10-08 23:05:19 -0400

    AGG/improve folderImgsModule UI

--- 01-scripts/appScripts/folderImgsModule.R
+++ 01-scripts/appScripts/folderImgsModule.R
@@ -9,6 +9,12 @@ folderImgsUI <- function(id){
               label = 'Select images',
               multiple = TRUE,
               accept=c('image/png', 'image/jpeg', 'image/tif')),
+    radioButtons(
+      ns("imgsPerRow"),
+      label = "# imgs per row",
+      choices = c(1, 2, 3, 4, 6),
+      selected = 3
+    ),
     uiOutput(ns('images'))
   )
 }
@@ -31,44 +37,54 @@ folderImgsServer <- function(
       
       output$images <- renderUI({
         if(is.null(input$files)) return(NULL)
+        
+        imgsPerRow <- input$imgsPerRow
+        divCols <- case_when(
+          imgsPerRow == 1 ~ "12",
+          imgsPerRow == 2 ~ "6",
+          imgsPerRow == 3 ~ "4",
+          imgsPerRow == 4 ~ "3",
+          imgsPerRow == 6 ~ "2"
+        )
+        
         image_output_list <- 
-          lapply(1:nrow(files()),
-                 function(i)
-                 {
-                   fileName <- paste0("name", i)
-                   imagename <- paste0("image", i)
-                   tags$div(
-                     class = "col-sm-3",
-                     textOutput(session$ns(fileName), container = h4),
-                     imageOutput(session$ns(imagename), height = "auto") # auto fixes the overlap
-                   )
-                 })
+          lapply(
+            1:nrow(files()),
+            function(i)
+            {
+              imagename <- paste0("image", i)
+              
+              thisFileName <- files()$name[i] %>%
+                path_file() %>%
+                path_ext_remove()
+              
+              tags$div(
+                class = paste0("col-xs-", divCols),# "col-xs-6 col-sm-4 col-md-3", # "col-sm-3",
+                h4(thisFileName),
+                imageOutput(session$ns(imagename), height = "auto") %>% # auto fixes the overlap
+                  tagAppendAttributes(class = 'myImages')
+              )
+            })
         
-        
-        div(
+        thisUI <- div(
           class = "container-fluid",
           div(
             class = "row",
             do.call(tagList, image_output_list)
           )
         )
+        
+        return(thisUI)
       })
       
       observe({
         if(is.null(input$files)) return(NULL)
         for (i in 1:nrow(files()))
         {
-          print(i)
+          # print(i)
           local({
             my_i <- i
             imagename = paste0("image", my_i)
-            fileName <- paste0("name", my_i)
-            outputWidth <- paste0("output_", imagename, "_width")
-            outputHeight <- paste0("output_", imagename, "_height")
-            print(imagename)
-            output[[fileName]] <- renderText({
-              files()$name[my_i]
-            })
             output[[imagename]] <- 
               renderImage({
                 list(src = files()$datapath[my_i],
