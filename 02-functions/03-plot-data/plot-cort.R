5b53b8f65af1908e4f54cf59e5ca237b3017a377
Amanda Gibson <gibsonag@umich.edu>
2022-03-07 10:03:06 -0500

    Add options to cort plot

--- 02-functions/03-plot-data/plot-cort.R
+++ 02-functions/03-plot-data/plot-cort.R
@@ -2,52 +2,80 @@ cortPlot <- function(
   df_long,
   pointSize = 1.2,
   fontSize = 11,
-  groupByVar = comboTrt,
-  lineTypeGuide = c("dotted", "dotted", "solid", "solid")
+  zoom_x = FALSE, #Zoom to a part of x axis
+  xmin = NULL,
+  xmax = NULL,
+  zoom_y = FALSE, #Zoom to a part of y axis
+  ymin = NULL,
+  ymax = NULL,
+  plotMean = TRUE,
+  plotSE = TRUE,
+  xBreaks = c(0, 5),
+  xLabels = c("pre", "post"),
+  lineTypeGuide = c("dotted", "dotted", "solid", "solid"),
+  positionDodge = 1.2,
+  groupVar = comboTrt
 ){
-  ggplot(
+  viz <- ggplot(
     df_long,
     aes(
       x = time,
       y = cort,
-      group = {{ groupByVar }}
+      group =  {{ groupVar }}
     )
   ) +
     geom_line(
       alpha = 0.4,
       # color = "black",
-      aes(group = mouseID, linetype = {{ groupByVar }}, color = {{ groupByVar }}),
-      position = position_dodge(1.2)
+      aes(group = mouseID, linetype =  {{ groupVar }}, color =  {{ groupVar }}),
+      position = position_dodge(positionDodge)
     ) +
     geom_point(
       # shape = 21, 
       alpha = 1, 
-      aes(fill={{ groupByVar }},group=mouseID, shape={{ groupByVar }}, color={{ groupByVar }}), 
-      position = position_dodge(1.2), 
+      aes(fill= {{ groupVar }},group=mouseID, shape= {{ groupVar }}, color= {{ groupVar }}), 
+      position = position_dodge(positionDodge), 
       size = pointSize
     ) +
-    addMeanHorizontalBar(
-      width = 1.4, 
-      addLineType = TRUE,
-      lineTypeName = "treatment",
-      lineTypeGuide = lineTypeGuide,
-      typeVar={{ groupByVar }},
-      color={{ groupByVar }}
-      )+
-    addMeanSE_vertBar(color={{ groupByVar }})+
-    comboTrtFillShape()+ ### IMPORTANT - Change based on needs
     theme_pubr() +
-    rremove("xlab") +
+    expand_limits(y = 0) +
+    coord_cartesian(if(zoom_x){xlim = c(xmin, xmax)}, if(zoom_y){ylim = c(ymin, ymax)}) +
+    # rremove("xlab") + ## seems like can't add back after if do this
     labs(
-      y = "corticosterone (ng/mL)"
+      y = "corticosterone (ng/mL)",
+      x = NULL
     ) +
     scale_x_continuous(
-      breaks = c(0, 5),
-      labels = c("pre", "post")
+      breaks = xBreaks, #c(0, 5),
+      labels = xLabels, #c("pre", "post")
     ) +
     textTheme(size = fontSize)+
     boxTheme()+
     guides()#linetype = "none")
+  
+  if(plotMean){
+    viz <- viz + addMeanHorizontalBar(
+      width = 1.4, 
+      addLineType = TRUE,
+      lineTypeName = "treatment",
+      lineTypeGuide = c("dotted", "dotted", "solid", "solid"),
+      typeVar= {{ groupVar }},
+      color= {{ groupVar }}
+    )
+  } else {
+    viz <- viz + labs(linetype = "treatment")
+  }
+  
+  if(plotSE){
+    viz <- viz + addMeanSE_vertBar(color= {{ groupVar }})
+  }
+  
+  if(deparse(substitute(groupVar)) == "comboTrt"){
+    viz <- viz + comboTrtFillShape()
+  } else {
+    viz <- viz + labs(color = "treatment", shape = "treatment", fill = "treatment")
+  }
+  return(viz)
 }
 baseCortPlot <- function(
   df_long,
